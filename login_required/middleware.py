7e99f6fc9736d3b398ac307990cb2adbaf07a2da
Pedro Walter <pedrohardware@gmail.com>
2017-07-20 02:29:50 -0300

    Removing hardcoded settings

--- login_required/middleware.py
+++ login_required/middleware.py
@@ -6,10 +6,23 @@ class LoginRequiredMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request):
+        available_settings = dir(settings)
+        if 'LOGIN_URL' not in available_settings:
+            raise Exception('LOGIN_URL must be defined at settings')
+        login_url = settings.LOGIN_URL
+
+        if 'LOGIN_EXEMPT_URLS' not in available_settings:
+            login_exempt_urls = []
+        else:
+            login_exempt_urls = settings.LOGIN_EXEMPT_URLS
+        login_exempt_urls.append(login_url)
+
         # No need to process URLs if user already logged in
         if request.user.is_authenticated():
             return self.get_response(request)
-        elif request.path in ["/login/", "/password_reset/", "/password_reset/done/"]:
+        # Let user go through exempt urls
+        elif request.path in login_exempt_urls:
             return self.get_response(request)
+        # Direct to login URL is user is not authenticated
         else:
-            return HttpResponseRedirect("/login")
+            return HttpResponseRedirect(login_url)
